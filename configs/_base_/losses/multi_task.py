200fb1ef01aefbaa42f62035f09dd59f41bc65c1
sandylaker <yawei.li@tum.de>
2022-01-30 00:20:09 +0100

    fix bugs for multi task loss

--- configs/_base_/losses/multi_task.py
+++ configs/_base_/losses/multi_task.py
@@ -1,5 +1,4 @@
 loss = dict(
     type='MultiTaskLoss',
     num_classes=15,
-    with_pos_weight=False,
 )
--- imba_explain/losses/multi_task.py
+++ imba_explain/losses/multi_task.py
@@ -2,10 +2,44 @@ from typing import Optional, Union
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import Tensor
 
 from .builder import LOSSES
-from .cross_entropy import binary_cross_entropy
+from .utils import weight_reduce_loss
+
+
+def multi_class_loss(pred: Tensor,
+                     label: Tensor,
+                     log_variance: torch.Tensor,
+                     weight: Optional[Tensor] = None,
+                     pos_weight: Optional[Tensor] = None,
+                     reduction: str = 'mean',
+                     avg_factor: Optional[float] = None) -> Tensor:
+    assert pred.dim() == label.dim()
+    assert log_variance.dim() == 1
+
+    # inter class weight, exp(- log_variance) = 1 / sigma^2. Please see the formulas in the original paper.
+    class_weight = torch.exp(-log_variance)
+    class_weight = class_weight.repeat(pred.size()[0], 1)
+
+    # manually multiply class_weight with raw bce loss,
+    # since the bce loss does not support back propagating through class_weight
+    loss = F.binary_cross_entropy_with_logits(pred, label, pos_weight=pos_weight, reduction='none')
+    loss = class_weight * loss
+
+    # apply sample-wise weight
+    if weight is not None:
+        assert weight.dim() == 1
+        weight = weight.float()
+        if pred.dim() > 1:
+            weight = weight.reshape(-1, 1)
+
+    loss = weight_reduce_loss(loss, weight=weight, reduction=reduction, avg_factor=avg_factor)
+
+    if reduction in ('mean', 'sum'):
+        loss += 0.5 * torch.sum(log_variance)
+    return loss
 
 
 @LOSSES.register_module()
@@ -15,7 +49,6 @@ class MultiTaskLoss(nn.Module):
     Args:
         num_classes: Number of classes.
         loss_weight: weight factor multiplied with the multi-task loss.
-        with_pos_weight: if True, the loss function will include learnable weights for positive answers of each class.
         reduction: The method to reduce the loss into a scalar.
         device: device to allocate the parameters.
 
@@ -23,7 +56,6 @@ class MultiTaskLoss(nn.Module):
 
     def __init__(self,
                  num_classes: int = 15,
-                 with_pos_weight: bool = False,
                  loss_weight: float = 1.0,
                  reduction: str = 'mean',
                  device: Optional[Union[str, torch.device]] = None) -> None:
@@ -34,11 +66,6 @@ class MultiTaskLoss(nn.Module):
 
         self.log_variance = nn.Parameter(torch.zeros(self.num_classes, device=device))
 
-        if with_pos_weight:
-            self.pos_weight = nn.Parameter(torch.ones(self.num_classes, device=device))
-        else:
-            self.pos_weight = None
-
     def forward(self,
                 cls_scores: Tensor,
                 label: Tensor,
@@ -48,21 +75,8 @@ class MultiTaskLoss(nn.Module):
         assert reduction_override in (None, 'none', 'mean', 'sum')
         reduction = (reduction_override if reduction_override else self.reduction)
 
-        # inter class weight, exp(- log_variance) = 1 / sigma^2. Please see the formulas in the original paper.
-        class_weight = torch.exp(-self.log_variance)
-        # intra class weight
-        pos_weight = torch.relu(self.pos_weight) if self.pos_weight is not None else None
-        loss_cls = binary_cross_entropy(
-            cls_scores,
-            label,
-            weight=weight,
-            reduction=reduction,
-            avg_factor=avg_factor,
-            class_weight=class_weight,
-            pos_weight=pos_weight)
-
-        if reduction in ('mean', 'sum'):
-            loss_cls += 0.5 * torch.sum(self.log_variance)
+        loss_cls = multi_class_loss(
+            cls_scores, label, self.log_variance, weight=weight, reduction=reduction, avg_factor=avg_factor)
 
         loss_cls = self.loss_weight * loss_cls
         return loss_cls
