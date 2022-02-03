e3b82cecbd1c866e0982a2083b260047a71e5ecd
sandylaker <yawei.li@tum.de>
2022-02-03 10:49:59 +0100

    add truncated densenet config

---
+++ configs/train_classifier/r50_nih/r50_nih_asymmetric_100e.py
@@ -0,0 +1,4 @@
+_base_ = [
+    '../../_base_/classifiers/resnet50.py', '../../_base_/datasets/nih_class_dataset.py',
+    '../../_base_/losses/asymmetric.py', '../../_base_/schedules/100e.py'
+]
---
+++ configs/train_classifier/r50_nih/r50_nih_bce_100e.py
@@ -0,0 +1,4 @@
+_base_ = [
+    '../../_base_/classifiers/resnet50.py', '../../_base_/datasets/nih_class_dataset.py', '../../_base_/losses/bce.py',
+    '../../_base_/schedules/100e.py'
+]
