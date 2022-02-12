443ddab67f812797e8e61550294f440fcebb5bea
sandylaker <yawei.li@tum.de>
2022-02-12 20:34:29 +0100

    add extremal perturbation for resnet50

---
+++ configs/explain_classifier/extr_pert_r50_nih.py
@@ -0,0 +1,3 @@
+_base_ = [
+    '../_base_/datasets/nih_detect_dataset.py', '../_base_/explain/extr_pert.py', '../_base_/classifiers/resnet50.py'
+]
--- configs/explain_classifier/gradcam_r50_nih.py
+++ configs/explain_classifier/gradcam_r50_nih.py
@@ -1,3 +1,5 @@
 _base_ = [
     '../_base_/datasets/nih_detect_dataset.py', '../_base_/explain/gradcam.py', '../_base_/classifiers/resnet50.py'
 ]
+
+attribution_method = dict(attr_normalizer=dict(type='MinMaxNormalizer'))
