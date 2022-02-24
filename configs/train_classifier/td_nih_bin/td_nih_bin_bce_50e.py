77f7d58bf034dd733637ac4d2142cc78295b8483
sandylaker <yawei.li@tum.de>
2022-02-24 21:55:27 +0100

    fix bugs in config

--- configs/train_classifier/td_nih_bin/td_nih_bin_bce_50e.py
+++ configs/train_classifier/td_nih_bin/td_nih_bin_bce_50e.py
@@ -1,4 +1,4 @@
 _base_ = [
     '../../_base_/classifiers/truncated_densenet.py', '../../_base_/datasets/nih_binary_dataset.py',
-    '../../_base_/losses/no_finding/bce.py', '../../_base_/schedules/50e.py'
+    '../../_base_/losses/bce.py', '../../_base_/schedules/50e.py'
 ]
--- configs/train_classifier/td_nih_bin/td_nih_bin_intra_50e.py
+++ configs/train_classifier/td_nih_bin/td_nih_bin_intra_50e.py
@@ -1,4 +1,4 @@
 _base_ = [
     '../../_base_/classifiers/truncated_densenet.py', '../../_base_/datasets/nih_binary_dataset.py',
-    '../../_base_/losses/no_finding/intra_wbce.py', '../../_base_/schedules/50e.py'
+    '../../_base_/losses/intra_wbce.py', '../../_base_/schedules/50e.py'
 ]
